a5de802af20bde0fb57d15148ef3c0fca2a98e2e
Daniel <daniel.weissen@gmx.ch>
2023-01-30 21:25:59 +0100

    asdf
    asdf

---
+++ README.md
@@ -0,0 +1,3 @@
+# TestMovement
+
+Developed with Unreal Engine 5
---
+++ Source/TestMovement/Public/TestMovementGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestMovementGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATestMovementGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATestMovementGameMode();
+};
+
+
+
